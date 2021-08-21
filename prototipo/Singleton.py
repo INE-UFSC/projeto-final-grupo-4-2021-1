9a1a0e1e9f11412e5f6ba0ef7699a675c228fa17
joaomai <joaomai2001@gmail.com>
2021-08-20 22:56:57 -0300

    typo singleton

--- prototipo/Singleton.py
+++ prototipo/Singleton.py
@@ -1,6 +1,6 @@
 from fighter.main_character.MainCharacter import MainCharacter
 from fighter.opponent.Opponent import Opponent
 
-class Si1ngleton:
+class Singleton:
     main_character: MainCharacter = None
     opponent : Opponent = None
