0397954c7f27c1bd04df66f11e29022384c96c9f
Enzo-Bassani <65874230+Enzo-Bassani@users.noreply.github.com>
2021-08-20 23:43:12 -0300

    Add Shrek files

--- prototipo/fighter/opponent/Opponent.py
+++ prototipo/fighter/opponent/Opponent.py
@@ -15,6 +15,9 @@ class Opponent(Fighter):
     def __init__(self, stats: Stats, hp: Resource, ap: Resource, equipment: Equipment, info: OpponentInfo, behavior: Behavior, skills: list = []):
         self.__info = info
         self.__behavior = behavior
+        self.__surf = pygame.Surface(size=(456, 200))
+        self.__image = pygame.image.load("prototipo/Images/ShrekSprite.png").convert_alpha()
+        self.__rect = self.__surf.get_rect(center=(400, 300))
         super().__init__(stats, hp, ap, equipment, skills)
 
     @staticmethod
@@ -30,10 +33,7 @@ class Opponent(Fighter):
         return self.__behavior
 
     def draw(self, surface):
-        surf = pygame.Surface(size=(100, 100))
-        surf.fill(pygame.Color("red"))
-        rect = surf.get_rect(center=(400, 300))
-        surface.blit(surf, rect)
+        surface.blit(self.__image, self.__rect)
 
     #def use_skill(self):
         #super().use_skill(self.__behavior.choose_skill)
