d454ac350fd0d049002a89d873c0fc9d8f1615c9
Enzo-Bassani <65874230+Enzo-Bassani@users.noreply.github.com>
2021-09-16 23:25:55 -0300

    Added base class Singleton, still not used.

--- prototipo/Singleton.py
+++ prototipo/Singleton.py
@@ -3,10 +3,18 @@ from fighter.opponent.Opponent import Opponent
 from room.Room import Room
 import pygame
 
-
 class Singleton:
     room: Room = None
     main_character: MainCharacter = None
     opponent: Opponent = None
     background = None
     screen_rect: pygame.Rect = None
+
+#ATENÇÃO TIME: É PRA IMPLEMENTAR ESPECIALIZAÇÕES DESSE AQUI
+class SingletonCertoQueEhPraImplementar(object):
+    __instance = None
+
+    def __new__(cls, *args):
+        if cls.__instance is None:
+            cls.__instance = object.__new__(cls)
+        return cls.__instance
