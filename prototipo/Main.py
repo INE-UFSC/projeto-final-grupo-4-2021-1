6b041bb8efb0c1734ae1f47410d58b8c1232f4fb
rafaelbcastilhos <rafaelbcastilhos@gmail.com>
2021-09-24 19:49:10 -0300

    fix and remove old singleton

--- prototipo/Main.py
+++ prototipo/Main.py
@@ -14,14 +14,11 @@ from states.OpponentPlayingState import OpponentPlayingState
 from states.EndState import EndState
 from creators.MainCharacterCreator import MainCharacterCreator
 from Game import Game
-from Singleton import Singleton
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 
 MainCharacterCreator.generate_test_character()
-Singleton.background = pygame.image.load("prototipo/assets/Background.png")
-Singleton.screen_rect = pygame.display.get_surface().get_rect()
 
 states = {
     "SPLASH": SplashState(),
@@ -29,13 +26,13 @@ states = {
     "OPTIONS": OptionsState(),
     "EQUIPMENT": EquipmentState(),
     "START_COMBAT": StartCombatState(),
-    "MAIN_CHARACTER_PLAYING": MainCharacterPlayingState(),
     "OPPONENT_PLAYING": OpponentPlayingState(),
     "END_COMBAT": EndCombatState(),
     "HEAL_ROOM": HealRoomState(),
     "TREASURE_ROOM": TreasureRoomState(),
     "INVENTORY": InventoryState(),
     "END": EndState(),
+    "MAIN_CHARACTER_PLAYING": MainCharacterPlayingState(),
 }
 
 game = Game(screen, states, "SPLASH")
